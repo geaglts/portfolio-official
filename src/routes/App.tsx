7d38be7d89a2c3567a9df215fc1f41eba8d5fa7c
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-05-21 15:17:30 -0500

    config sass

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 import logo from '@icons/logo.svg';
-import '@styles/App.css';
+import styles from '@styles/App.module.scss';
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+		<div className={styles.app}>
+			<header className={styles.appHeader}>
+				<img src={logo} className={styles.appLogo} alt="logo" />
 				<p>Hello Vite + React!</p>
 				<p>
-					<button type="button" onClick={() => setCount((count) => count + 1)}>
+					<button
+						className="primary"
+						type="button"
+						onClick={() => setCount((count) => count + 1)}
+					>
 						count is: {count}
 					</button>
 				</p>
@@ -19,12 +23,17 @@ function App() {
 					Edit <code>App.tsx</code> and save to test HMR updates.
 				</p>
 				<p>
-					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					<a
+						className={styles.appLink}
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
 						Learn React
 					</a>
 					{' | '}
 					<a
-						className="App-link"
+						className={styles.appLink}
 						href="https://vitejs.dev/guide/features.html"
 						target="_blank"
 						rel="noopener noreferrer"
