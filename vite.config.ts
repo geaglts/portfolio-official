512311ffd9a741448db8432299fdd93d71a17916
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-05-22 14:48:41 -0500

    fix path bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "https://geaglts.github.io/",
 	plugins: [react(), viteTypescriptPaths()],
 });
