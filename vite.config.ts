5d880524639fa4ea8cb654d88b682744f93d697a
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-05-22 15:18:32 -0500

    fix path bug with custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: 'https://geaglts.github.io/portfolio-official/',
+	base: '/',
 	plugins: [react(), viteTypescriptPaths()],
 });
